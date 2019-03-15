3e468a1f71b8219721a851fdfcfc40ab87590e8c
lite-authenticator-7506 <kurianvijay7@gmail.com>
2019-03-15 13:48:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,27 @@
 # Write your code here.
 katz_deli = []
+
+
 def take_a_number(katz_deli, a)
   puts "your name is #{a} and you number is #{katz_deli}"
 end
 
-def line(array, element=0)
-if array.length > 0 
-puts array.index(element)
-else 
-  puts "The line is currently empty"
-end
-end
+learn
 
 
-def now_serving(i)
-next= i.length + 1
-end
+
+#def line(array, element=0)
+#if array.length > 0 
+#puts array.index(element)
+#else 
+#  puts "The line is currently empty"
+#end
+#end
+
+
+#def now_serving(i)
+#next= i.length + 1
+#end
   
   
    
