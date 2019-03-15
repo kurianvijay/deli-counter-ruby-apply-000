dc488bc10a185ed159b5dcfe5837022ead13d1c0
lite-authenticator-7506 <kurianvijay7@gmail.com>
2019-03-15 14:01:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ katz_deli = []
 def take_a_number(array, name)
 array << name  
 array.length
- puts "your name is #{name} and you number is #{array.length}"
+ puts "Welcome, #{name}. and you are number #{array.length} in line."
 end
 
 # add new incoming element
